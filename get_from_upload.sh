a681a10a563212d40e59d75f7c8cd45d465fd218
zdanozdan <tomasz.zdanowski@mikran.com>
2019-05-22 17:39:58 +0000

    new theme created

---
+++ get_from_upload.sh
@@ -0,0 +1,14 @@
+mkdir -p admin/language/en-gb/extension/theme/
+cp ../upload/admin/language/en-gb/extension/theme/mikran.php admin/language/en-gb/extension/theme/
+
+#mkdir -p admin/language/pl-PL/extension/theme/
+#cp ../upload/admin/language/pl-PL/extension/theme/mikran.php admin/language/pl-PL/extension/theme/
+
+mkdir -p admin/view/template/extension/theme
+cp ../upload/admin/view/template/extension/theme/mikran.twig admin/view/template/extension/theme/
+
+mkdir -p admin/controller/extension/theme
+cp ../upload/admin/controller/extension/theme/mikran.php admin/controller/extension/theme
+
+mkdir -p catalog/view/theme/mikran
+cp -R ../upload/catalog/view/theme/mikran catalog/view/theme/
